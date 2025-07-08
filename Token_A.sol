9e25a66cb7d05d024f3f80777f63db1bf9feeaae
Ruyasi Chanove <64982427+Pengwin-dev@users.noreply.github.com>
2025-07-08 19:16:56 -0400

    Create Token_A.sol
    Created Token A

---
+++ Token_A.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v5.0.2/contracts/token/ERC20/ERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v5.0.2/contracts/access/Ownable.sol";
+
+/**
+ * @title AlphaToken
+ * @dev This is the first ERC-20 token for the SimpleDEX.
+ * It inherits from OpenZeppelin's ERC20 and Ownable contracts.
+ * The initial supply is minted to the deployer of the contract.
+ */
+contract AlphaToken is ERC20, Ownable {
+    /**
+     * @dev Constructor that sets the token name and symbol.
+     * It also mints the initial total supply to the contract deployer.
+     */
+    constructor(address initialOwner) ERC20("AlphaToken", "ALPHA") Ownable(initialOwner) {
+        // Mint 1,000,000 tokens to the deployer's address.
+        // Since the token has 18 decimals, we need to add 18 zeros.
+        _mint(msg.sender, 1_000_000 * (10**decimals()));
+    }
+
+    /**
+     * @dev Creates `amount` new tokens and assigns them to `to`.
+     * This function can only be called by the owner of the contract.
+     * @param to The address that will receive the minted tokens.
+     * @param amount The amount of tokens to mint.
+     */
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+}
